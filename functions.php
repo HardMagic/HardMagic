57f56d98d72330fcc26443ec873d97c5f9633e89
Matt Hackney <matt@themov.org>
2014-10-12 21:13:34 +0000

    INI Settings

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ Keep in mind these are add on functions for Brooklyn  Theme
 show_admin_bar(false); 
 
 @ini_set( 'upload_max_size' , '64M' );
-2
+
 @ini_set( 'post_max_size', '64M');
-3
+
 @ini_set( 'max_execution_time', '300' );
