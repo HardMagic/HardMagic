fbda18164a03e72928b5099e5ebea9fadc8286cc
Matt Hackney <matt@themov.org>
2014-10-13 03:00:19 +0000

    Favicon

--- functions.php
+++ functions.php
@@ -11,3 +11,10 @@ show_admin_bar(false);
 @ini_set( 'post_max_size', '64M');
 
 @ini_set( 'max_execution_time', '300' );
+
+
+// add a favicon to your site
+function blog_favicon() {
+	echo '<link rel="Shortcut Icon" type="image/x-icon" href="'.get_bloginfo('stylesheet_directory').'/images/favicon.ico" />' . "\n";
+}
+add_action('wp_head', 'blog_favicon');
